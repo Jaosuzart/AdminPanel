85baf72fdeb6a79e5ffb2c38c2a579d3b937aacb
Jaosuzart <joaomarcelosuzartcastro@gmail.com>
2026-09-03 10:18:48 -0300

    Enviando atualizacao ao github

--- api/app.js
+++ api/app.js
@@ -9,13 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Inicializa a conexão com o banco de dados (promisified)
 initDB();
 
-// Registra as rotas da API
 app.use('/api', authRoutes);
 
-// Middleware global de tratamento de erros, sempre o último a ser registrado
 app.use(errorHandler);
 
 export default app;
